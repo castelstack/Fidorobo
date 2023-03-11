bd37d82e1d15054a9e4b6a0b3b301072de6c51b9
castelstackk <okafor.henry@smitiv.co>
2023-03-11 19:53:20 +0100

    header and font-fixes

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const Logo = () => {
     return (
-        <h2 className='font-bold text-[32px]'>
+        <h2 className='font-bold md:text-[32px] text-[23px]   '>
           FieldRobo  
         </h2>
     );
