95d1ecb082f69e635eeb7cbd75016dc678d11ab1
castelstackk <okafor.henry@smitiv.co>
2023-03-11 19:52:08 +0100

    margin fixes

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,21 @@
-import '@/styles/globals.css'
+import '@/styles/globals.css';
+import { DM_Sans } from 'next/font/google';
+
+const dmsans = DM_Sans({
+  weight: ['400', '500','700'],
+  style: ['normal', 'italic'],
+  subsets: ['latin'],
+});
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <style jsx global>{`
+        html {
+          font-family: ${dmsans.style.fontFamily};
+        }
+      `}</style>
+      <Component {...pageProps} />
+    </>
+  );
 }
