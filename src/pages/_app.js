12ee8ddf71357470b37df922413306dabfee173c
castelstackk <okafor.henry@smitiv.co>
2023-03-28 15:18:36 +0100

    font fixes

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,20 +1,41 @@
 import '@/styles/globals.css';
 import { Provider } from 'jotai';
-import { DM_Sans } from 'next/font/google';
 import { ToastProvider } from 'react-toast-notifications';
+import localFont from 'next/font/local'
 
-const dmsans = DM_Sans({
-  weight: ['400', '500', '700'],
-  style: ['normal', 'italic'],
-  subsets: ['latin'],
-});
+// Font files can be colocated inside of `pages`
+
+const DM_Sans = localFont({
+  src: [
+    {
+      path: './DMSans-Regular.woff',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: './DMSans-Italic.woff',
+      weight: '400',
+      style: 'italic',
+    },
+    {
+      path: './DMSans-Medium.woff',
+      weight: '700',
+      style: 'normal',
+    },
+    {
+      path: './DMSans-Bold.woff',
+      weight: '700',
+      style: 'italic',
+    },
+  ],
+})
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <style jsx global>{`
         html {
-          font-family: ${dmsans.style.fontFamily};
+          font-family: ${DM_Sans.style.fontFamily};
         }
       `}</style>
       <Provider>
